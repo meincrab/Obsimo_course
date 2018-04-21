70cee1a5c843ba05936c02fd6fd3ef3be28589b6
K2679 <k2679@student.jamk.fi>
2018-04-21 20:55:44 +0300

    url gen fix

--- KOODI/opettajanSivut/luoUrl.php
+++ KOODI/opettajanSivut/luoUrl.php
@@ -1,13 +1,17 @@
 <?php
-	require_once("../config.php");
-	$username = $_POST['nimi'];
-	$token = sha1(uniqid($username, true));
-	$stmt = $db->prepare("INSERT INTO pending_users (username,token,tstamp) VALUES (:fi,:f2,NOW())");
-	$stmt->execute(array(':f1' => $username, ':f2' => $token));
-	
-	$url = "http://167.99.85.3/gg/KOODI/yritys.php?token=$token";
-	echo '<script language="javascript">';
-    echo 'alert(" ' $url ' ")';
-    echo '</script>';
-	
+require_once("../config.php");
+$username = $_POST['nimi'];
+$token = sha1(uniqid($username, true));
+$query = $db->prepare(
+	"INSERT INTO pending_users (username, token, tstamp) VALUES (?, ?, ?)"
+);
+$query->execute(
+    array(
+        $username,
+        $token,
+        $_SERVER["REQUEST_TIME"]
+    )
+);
+$url = "http://example.com/activate.php?token=$token";
+echo "<script type='text/javascript'>alert('$url');</script>";
 ?>
