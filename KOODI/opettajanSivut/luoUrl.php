d64e6fd36915ae1b58186e7de9ef87fd1c196d7d
K2679 <k2679@student.jamk.fi>
2018-04-22 16:38:05 +0300

    fix sql/php

--- KOODI/opettajanSivut/luoUrl.php
+++ KOODI/opettajanSivut/luoUrl.php
@@ -1,17 +1,17 @@
 <?php
-require_once("config.php");
-$username = $_POST['nimi'];
-$token = sha1(uniqid($username, true));
-$query = $db->prepare(
-	"INSERT INTO pending_users (username, token, tstamp) VALUES (?, ?, ?)"
-);
-$query->execute(
-    array(
-        $username,
-        $token,
-        $_SERVER["REQUEST_TIME"]
-    )
-);
-$url = "http://167.99.85.3/gg/KOODI/yritys/yritys.php?token=$token";
-echo "<script type='text/javascript'>alert('$url');</script>";
+	require_once("config.php");
+	if ($conn->connect_error) {
+               die("Connection failed: " . $conn->connect_error);
+    } else {
+	$username = $_POST['nimi'];
+	$token = sha1(uniqid($username, true));
+	$query = $mysqli->prepare(
+		"INSERT INTO pending_users (username, token, tstamp) VALUES (?, ?, ?)"
+	);
+	$query->bind_params($username, $token, $_SERVER["REQUEST_TIME"] )
+	$query->execute();
+	
+	$url = "http://167.99.85.3/gg/KOODI/yritys/yritys.php?token=$token";
+	echo "<script type='text/javascript'>alert('$url');</script
+	}
 ?>
