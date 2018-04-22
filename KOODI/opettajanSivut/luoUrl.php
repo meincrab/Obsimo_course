b4a9148c7fcbb1195298e9aa4417162224b81523
K2679 <k2679@student.jamk.fi>
2018-04-22 16:46:47 +0300

    fix typos

--- KOODI/opettajanSivut/luoUrl.php
+++ KOODI/opettajanSivut/luoUrl.php
@@ -1,19 +1,19 @@
 <?php
 	require_once("config.php");
+	if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+	}
 	date_default_timezone_set("Europe/Helsinki");
-	if ($conn->connect_error) {
-			echo "<script type='text/javascript'>alert('CONNECTION FAILED');</script
-            die("Connection failed: " . $conn->connect_error);
-    } else {
 	$username = $_POST['nimi'];
 	$token = sha1(uniqid($username, true));
-	$query = $mysqli->prepare(
+	$timestamp = time();
+	$stmt = $mysqli->prepare(
 		"INSERT INTO pending_users (username, token, tstamp) VALUES (?, ?, ?)"
 	);
-	$query->bind_params($username, $token, $_SERVER["REQUEST_TIME"] )
-	$query->execute();
+	$stmt->bind_params($username, $token, $timestamp );
+	$stmt->execute();
 	
 	$url = "http://167.99.85.3/gg/KOODI/yritys/yritys.php?token=$token";
-	echo "<script type='text/javascript'>alert('$url');</script
-	}
+	echo "<script type='text/javascript'>alert('$url');</script>";
 ?>
