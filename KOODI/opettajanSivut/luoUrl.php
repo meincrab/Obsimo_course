4948c25a9e182aed480c2f184aa628fabde0e0a7
K2679 <k2679@student.jamk.fi>
2018-04-22 17:14:08 +0300

    add debugging code

--- KOODI/opettajanSivut/luoUrl.php
+++ KOODI/opettajanSivut/luoUrl.php
@@ -4,13 +4,18 @@
     $query = "INSERT INTO pending_users (username, token, tstamp) VALUES (?,?,?)";
 	$stmt = $mysqli->prepare($query);
         
-	$stmt->bind_params("sss", $username, $token, $timestamp);
+	$stmt->bind_params($username, $token, $timestamp);
                              
     $username = $_POST['nimi'];
 	$token = sha1(uniqid($username, true));
-	$timestamp = time();
+	$timestamp = time();    
 
 	$stmt->execute();
+    if (!$result) {
+    $message  = 'Invalid query: ' . mysql_error() . "\n";
+    $message .= 'Whole query: ' . $query;
+    die($message);
+    }
 	
 	$url = "http://167.99.85.3/gg/KOODI/yritys/yritys.php?token=$token";
 	echo "<script type='text/javascript'>alert('$url');</script>";
